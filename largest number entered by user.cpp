80179ea12fadf7e25fd405f477800d71739a0c30
M MarisUlaganathan <51428822+maris8838@users.noreply.github.com>
2019-07-09 12:34:13 +0530

    Update largest number entered by user.cpp

--- largest number entered by user.cpp
+++ largest number entered by user.cpp
@@ -1,13 +1,12 @@
-  #include <iostream>
+ #include <iostream>
     using namespace std;
     int main()
     {
         int a, b, c;
-        cout << "Enter three numbers: ";
         cin >> a >> b >> c;
-        if(a > b && a > c)
+        if(a >= b && a >= c)
             cout << a;
-        else if (b > a && b > c)
+        else if (b >= a && b >= c)
             cout << b;
         else
             cout << c;
