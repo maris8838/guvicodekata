595bb29ec6beab3b9b2ae657c7a753ea0ff92619
M MarisUlaganathan <51428822+maris8838@users.noreply.github.com>
2019-07-09 11:53:30 +0530

    Update even or odd.cpp

--- even or odd.cpp
+++ even or odd.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
-    using namespace std;
-    int main()
-    {
-       int a;
-        cin>>a;
-       if (a%2==0){
-           cout<<"Even";
-       }
-       else
-       {
-           cout<<"Odd";
-       }
-        
-        return 0;
+using namespace std;
+int main()
+{ 
+    int a;
+    cin>>a;
+    if(a>0){
+    if(a%2==0){
+        cout<<"Even";
     }
+    else{
+        cout<<"Odd";
+    }
+    }
+    else
+    {
+        cout<<"Invalid";
+    }
+    return 0;
+}
