ac011ac4639590758d4224e06260d387fe02e342
M MarisUlaganathan <51428822+maris8838@users.noreply.github.com>
2019-07-09 10:36:08 +0530

    Add files via upload

---
+++ positive.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int number;
+    cin>>number;
+    
+    if ( number > 0)
+    {
+        cout << "Positive";
+    }
+    else if (number < 0)
+    {
+        cout<<"Negative";
+    }
+    else
+    {
+        cout << "Zero";
+    }
+   
+    return 0;
+}
