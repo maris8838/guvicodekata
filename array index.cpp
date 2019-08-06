f7a4b8af9d8b328c0b847728a36a895a9d8e2508
M MarisUlaganathan <51428822+maris8838@users.noreply.github.com>
2019-08-07 01:57:28 +0530

    Add files via upload

---
+++ array index.cpp
@@ -0,0 +1,13 @@
+ #include <iostream>
+    using namespace std;
+    int main()
+    {
+      int i, n ,a[10], min=0, sort=0;
+      cin>>n;
+      for(i=0;i<n;i++)
+      {
+        cin>>a[i];
+        cout<<a[i]<<" "<<i;
+          
+      }
+    }
