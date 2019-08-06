f562465568565c72cda4d2b9e06a93721c5a74b5
M MarisUlaganathan <51428822+maris8838@users.noreply.github.com>
2019-08-06 21:24:36 +0530

    Add files via upload

---
+++ 1st 5 multiplies.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,num,multiply;
+    cin>>num;
+    
+    for(i=1; i<6;i++){
+        multiply = i * num;
+       cout<<" "<<multiply; 
+    }
+    
+    return 0;
+}
