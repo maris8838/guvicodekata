be516a6c62db3f9d16333aa1a782afdf547c892a
M MarisUlaganathan <51428822+maris8838@users.noreply.github.com>
2019-08-06 23:55:06 +0530

    Add files via upload

---
+++ array max.cpp
@@ -0,0 +1,14 @@
+    #include<iostream>
+    using namespace std;
+    int main(){
+        int i,n,arr[10000];
+        cin>>n;
+        for(i=0; i<n; i++){
+            cin>>arr[i];
+        }
+        for(i=1; i<n; i++){
+            if(arr[0] < arr[i])
+            arr[0] = arr[i];
+        }
+        cout <<arr[0];
+    }
