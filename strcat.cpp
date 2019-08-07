a95b79508cec758cb6e69d0034df940b3ec3ccec
M MarisUlaganathan <51428822+maris8838@users.noreply.github.com>
2019-08-08 00:42:08 +0530

    Add files via upload

---
+++ strcat.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    char str1[50],str2[50];
+    
+    cin>>str1>>str2;
+    
+    strcat (str1, str2);
+        cout<<""<<str1<<endl;
+        
+}
