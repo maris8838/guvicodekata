4e50edacfc3b99a32e23d92a8857db7c8a80755c
M MarisUlaganathan <51428822+maris8838@users.noreply.github.com>
2019-08-07 10:21:27 +0530

    Add files via upload

---
+++ add two numbers and print even or odd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int num1,num2,sum=0,reult;
+  cin>>num1;
+  cin>>num2;
+  sum=num1+num2;
+  if(sum%2==0){
+      cout<<"even";
+    }
+    else
+    {
+        cout<<"odd";
+    }
+}
