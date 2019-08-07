1681997b9fab5fc965efd18662f07b817b7508db
M MarisUlaganathan <51428822+maris8838@users.noreply.github.com>
2019-08-08 00:04:48 +0530

    Add files via upload

---
+++ sting_k_times.cpp
@@ -0,0 +1,13 @@
+  #include<iostream>
+   using namespace std;
+   int main(){
+   char s[100];
+   int k;
+   cin>>s;
+   cin>>k;
+   for(int i=1;i<=k;i++){
+   cout<<" "<<s;
+   }
+   return 0;
+   }
+    
