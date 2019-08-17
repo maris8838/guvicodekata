225419da0e772aab065e7ffef958357b100fa473
M MarisUlaganathan <51428822+maris8838@users.noreply.github.com>
2019-08-17 22:29:41 +0530

    Add files via upload

---
+++ set_8_q_3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int N, L, R;
+    cin>>N;
+    cin>>L;
+    cin>>R;
+    if(N>L && N<R){
+        cout<<"yes";
+    }else{
+        cout<<"no";
+    }
+}
